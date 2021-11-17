a705bab6df88e7de1623c6216306e4f0cc9ded9b
Kaushik Devireddy <kdev@ucla.edu>
2021-11-16 17:58:18 -0800

    Update check_changes.py

--- check_changes.py
+++ check_changes.py
@@ -36,7 +36,7 @@ def post_findings_to_github(analysis_table):
 	headers = {'Content-Type': 'application/json', 'Authorization': f'token {GITHUB_TOKEN}'}
 	data_string = f"""<h3>Dassana has detected changes in your tracked CloudFormation template</h3></br>Review the following to avoid service disruptions and/or security risks <hr/></br><details><summary>View Dassana's Change Analysis</summary></br>
 	
-	{analysis_table}</details>"""
+{analysis_table}</details>"""
 	data = {'body':data_string}
 				
 	r = requests.post(url = pr_url, data = dumps(data), headers = headers)
