ce830af4fe143b1bfc61ce2e6520a563b04ed52b
Kaushik Devireddy <kdev@ucla.edu>
2021-11-16 23:33:18 -0800

    Update check_changes.py

--- check_changes.py
+++ check_changes.py
@@ -29,7 +29,6 @@ api_request_headers = {
   'Content-Type': 'application/json',
   'Origin': 'https://editor.dassana.io',
   'Referer': 'https://editor.dassana.io/',
-  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
   'x-api-key': API_KEY,
   'x-dassana-cache': 'false'
 }
